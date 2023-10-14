777a9acd13b6046f9066108c1e8c598a49040172
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-10-14 23:11:09 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
+import { LitElement, html } from "https://esm.sh/lit@3.0.0";
+import { customElement } from "https://esm.sh/lit@3.0.0/decorators.js";
 
+@customElement('app')
+export class App extends LitElement {
+    render() {
+        return html`
+            <p>Hello from my template.</p>
+        `
+    }
+}
